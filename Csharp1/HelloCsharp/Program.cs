a4e1fcaadc7bd7ed30baa3916beeab4641e67b24
lpzju <liupeng.hust@foxmail.com>
2024-07-12 09:45:07 +0800

    readme

---
+++ Csharp1/HelloCsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello Csharp");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
